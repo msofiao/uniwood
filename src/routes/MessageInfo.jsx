713aba43b1311a1416b30daa544e23a42a934ea4
msofiao <146215686+msofiao@users.noreply.github.com>
2024-03-26 22:59:52 +0800

    scss edit

--- src/routes/MessageInfo.jsx
+++ src/routes/MessageInfo.jsx
@@ -3,11 +3,11 @@ import Sidebar from "../components/Sidebar";
 import Convolist from "../components/Convolist";
 import Chat from "../components/Chat";
 import Info from "../components/Info";
-import "../messengerinfo.scss";
+import "../messenger.scss";
 
 const MessageInfo = () => {
   return (
-    <div className="messageinfo">
+    <div className="messenger">
       <Sidebar />
       <Convolist />
       <Chat />
