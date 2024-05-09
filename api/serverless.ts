4d1cd4ed45223dc8e914446f122bf96d870dbadf
Bryan Gonzales <brynrgnzls@gmail.com>
2024-05-09 13:06:23 +0800

    [UPDATE] moved serverless.ts file to api

---
+++ api/serverless.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import path from "node:path";
+
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+dotenv.config({ path: path.resolve(import.meta.url, "../.env") });
+
+const app = Fastify({
+  logger: true,
+});
+
+app.register(import("../server/app"), { prefix: "/" });
+
+export default async (req: FastifyRequest, res: FastifyReply) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
