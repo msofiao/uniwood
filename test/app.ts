a9cb3ab3e8624561994f8078f0bc7570f400c151
Bryan Gonzales <brynrgnzls@gmail.com>
2024-05-09 18:18:14 +0800

    [TEST] add cors

--- test/app.ts
+++ test/app.ts
@@ -5,6 +5,13 @@ export default function App(
   _options: any,
   done: () => void,
 ) {
+  // ===== Core Plugin ===== //
+  instance.register(import("@fastify/cors"), {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  });
+
   instance.get("/test", async (_req, res) => {
     return res.code(200).send({ status: "success", message: "Test Route" });
   });
